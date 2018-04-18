7b17679315f55b879c68e466a983bbe3e39ebf90
Manfred Karrer <mk@nucleo.io>
2018-04-17 21:04:22 -0500

    Apply refactorings to headless apps
    - Add HeadlessAppWithP2PModule for headless apps which do not use wallet
    - Add gracefulShutDown to HeadlessExecutable
    - Update Monitor, SeedNode and Statistics to latest refactorings
    - Remove BisqEnvironment from BisqApp
    - Add startApplication method to BisqApp
    - Set up bisqEnvironment in BisqExecutable
    - Add addCapabilities method
    - Add more comments

--- src/main/java/bisq/seednode/SeedNodeMain.java
+++ src/main/java/bisq/seednode/SeedNodeMain.java
@@ -19,49 +19,42 @@ package bisq.seednode;
 
 import bisq.core.app.BisqEnvironment;
 import bisq.core.app.BisqExecutable;
-import bisq.core.app.HeadlessExecutable;
+import bisq.core.app.ExecutableForAppWithP2p;
+import bisq.core.app.ModuleForAppWithP2p;
 
 import bisq.common.UserThread;
 import bisq.common.app.AppModule;
+import bisq.common.app.Capabilities;
+import bisq.common.setup.CommonSetup;
 
 import joptsimple.OptionSet;
-
-import com.google.inject.Injector;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class SeedNodeMain extends HeadlessExecutable {
-    private BisqEnvironment bisqEnvironment;
+public class SeedNodeMain extends ExecutableForAppWithP2p {
     private SeedNode seedNode;
 
     public static void main(String[] args) throws Exception {
-        // We don't want to do the full argument parsing here as that might easily change in update versions
-        // So we only handle the absolute minimum which is APP_NAME, APP_DATA_DIR_KEY and USER_DATA_DIR
         BisqEnvironment.setDefaultAppName("bisq_seednode");
 
-        if (BisqExecutable.setupInitialOptionParser(args)) {
-            // For some reason the JavaFX launch process results in us losing the thread context class loader: reset it.
-            // In order to work around a bug in JavaFX 8u25 and below, you must include the following code as the first line of your realMain method:
-            Thread.currentThread().setContextClassLoader(SeedNodeMain.class.getClassLoader());
-
+        if (BisqExecutable.setupInitialOptionParser(args))
             new SeedNodeMain().execute(args);
-        }
     }
 
     @Override
     protected void doExecute(OptionSet options) {
         super.doExecute(options);
 
-        checkMemory(bisqEnvironment, seedNode);
+        checkMemory(bisqEnvironment, this);
+        CommonSetup.setup(this);
 
         keepRunning();
     }
 
     @Override
-    protected void setupEnvironment(OptionSet options) {
-        bisqEnvironment = getBisqEnvironment(options);
-        SeedNode.setEnvironment(bisqEnvironment);
+    protected void addCapabilities() {
+        Capabilities.addCapability(Capabilities.Capability.SEED_NODE.ordinal());
     }
 
     @Override
@@ -69,6 +62,7 @@ public class SeedNodeMain extends HeadlessExecutable {
         UserThread.execute(() -> {
             try {
                 seedNode = new SeedNode();
+                onApplicationLaunched();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -76,14 +70,28 @@ public class SeedNodeMain extends HeadlessExecutable {
     }
 
     @Override
+    protected void onApplicationLaunched() {
+        super.onApplicationLaunched();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // We continue with a series of synchronous execution tasks
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
     protected AppModule getModule() {
-        //TODO not impl yet
-        return null;
+        return new ModuleForAppWithP2p(bisqEnvironment);
     }
 
     @Override
-    protected Injector getInjector() {
-        //TODO not impl yet
-        return null;
+    protected void applyInjector() {
+        super.applyInjector();
+
+        seedNode.setInjector(injector);
+    }
+
+    @Override
+    protected void startApplication() {
+        seedNode.startApplication();
     }
 }
