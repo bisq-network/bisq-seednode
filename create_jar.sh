632b1edf9cbb2dc7ee8edcf94cecb52aecc32c5b
Manfred Karrer <mk@nucleo.io>
2018-06-12 22:03:20 +0200

    Update build script

--- create_jar.sh
+++ create_jar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gradlew --include-build ../bisq-common --include-build ../bisq-assets --include-build ../bisq-p2p --include-build ../bisq-core build -x test shadowJar
+./gradlew build -x test shadowJar
